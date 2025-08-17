1f539936aea14a0ff4fae24ab349661519f8a01b
Faiza Ungguh <otemoos@outlook.com>
2025-08-17 11:06:28 +0700

    mutable variabel

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 fn main() {
     /*
-     * immutable pada variabel, gunakan {} untuk setiap variabel
-     * maka ketika mencoba mengubah nilai variabel yang sudah ada akan memunculkan pesan error
+     * mutable pada variabel, gunakan {} untuk setiap variabel
+     * maka ketika mencoba mengubah nilai variabel yang sudah ada tidak ada peringatan error
      */
-    let number = 1;
-    let message = "hello";
-    println!("message number {}: {}", number, message);
+    let mut number = 1;
+    let message1 = "hello";
+    println!("message number {}: {}", number, message1);
+
+    number = 2;
+    let message2 = "world";
+    println!("message number {}: {}", number, message2);
+
+    number = 3;
+    let message3: i8 = 24;
+    println!("message number {1}: {0}", message3, number);
+
+    println!("message number {}: {}", number, message3);
+    println!("message number {0}: {1}", number, message3);
+    println!("message number {1}: {0}", message3, number);
 }
