f0a60306f2df0184fca9aad4dbcb30a7a04c0caf
Faiza Ungguh <otemoos@outlook.com>
2025-08-17 08:38:55 +0700

    tes variabel

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let cek = "tes nilai variabel";
+    /*
+     * println! harus menyertakan ("{}", let) untuk dipanggil atau menghasilkan error
+     */
+    println!("{}", cek);
 }
