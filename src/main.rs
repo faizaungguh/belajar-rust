8c4215db47e64d2a7acb4a0e4f57d39c86ae5efe
Faiza Ungguh <otemoos@outlook.com>
2025-08-17 17:18:10 +0700

    integer

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,18 @@
 fn main() {
     /*
-     * variabel tanpa didefinisikan tipenya
+     * signed integer
+     * i8 = −(2^7) hingga (2^7)-1
+     * i16 = −(2^15) hingga (2^15)-1
+     * i32 = −(2^31) hingga (2^31)-1
+     * i64 = −(2^63) hingga (2^63)-1
+     * i128 = −(2^127) hingga (2^127)-1
      */
-    let number = 1;
-    let message1 = "hello";
-    println!("message number {}: {}", number, message1);
+    let numerik1 = 24;
+    // let numerik2: i8 = 128; /* akan muncul error, karena lebih dari 127 */
+    let numerik3: i64 = 12;
+    let min_i128 = i128::MIN;
+    let max_i128 = i128::MAX;
+    println!("{} | {}", min_i128, max_i128);
 
-    /*
-     * multivariabel
-     */
-    let (angka, kata) = (23, "Yo");
-    println!("angkanya : {0}", angka);
-    println!("katanya : {0}", kata);
-
-    /*
-     * multivariabel dengan didefinisikan tipenya
-     */
-    let (var5, mut var6, var7): (i8, i8, i8) = (64, 12, 4);
-    println!("var5: {0}", var5);
-    println!("var6: {0}", var6);
-    var6 = 24;
-    println!("var6: {0}", var6);
-    println!("var7: {0}", var7);
+    println!("{} | {}", numerik1, numerik3);
 }
